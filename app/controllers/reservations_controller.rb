618760f85d1590cfa6fb6197be2e3a9b03c364f0
hanzawa825 <157540704+hanzawa825@users.noreply.github.com>
2025-02-03 00:14:16 +0900

    fix/予約作成や一覧表示の変更

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,6 @@
 class ReservationsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_reservation, only: [:show]
+  before_action :set_reservation, only: [:show, :destroy, :update]
 
   def new
     @room = Room.find_by(id: params[:room_id])
@@ -11,17 +11,16 @@ class ReservationsController < ApplicationController
     end
   end
 
-  def confirm
-    @room = Room.find_by(id: params[:room_id])
-    if @room.nil?
-      redirect_to rooms_path, alert: '該当する部屋が見つかりませんでした。'
-      return
-    end
+  def show
+    @room = @reservation.room
+  end
 
-    @reservation = @room.reservations.new(reservation_params.merge(user_id: current_user.id))
-    if @reservation.invalid?
-      flash.now[:alert] = @reservation.errors.full_messages.join(', ')
-      render :new
+  def confirm
+    @reservation = Reservation.find_by(id: params[:id])
+    if @reservation.nil?
+      redirect_to reservations_path, alert: '予約が見つかりませんでした。'
+    else
+      @room = @reservation.room
     end
   end
 
@@ -31,24 +30,23 @@ class ReservationsController < ApplicationController
       redirect_to rooms_path, alert: '該当する部屋が見つかりませんでした。'
       return
     end
-  
-    # パラメータから予約情報を設定
+
     @reservation = @room.reservations.new(reservation_params.merge(user_id: current_user.id))
-  
-    if params[:reservation][:step] == 'confirm' # 確定処理
-      if @reservation.save
-        redirect_to reservations_path, notice: '予約が確定しました。'
-      else
-        flash.now[:alert] = @reservation.errors.full_messages.join(', ')
-        render :confirm
-      end
-    else # 確認画面の表示
-      if @reservation.valid?
-        render :confirm
-      else
-        flash.now[:alert] = @reservation.errors.full_messages.join(', ')
-        render :new
-      end
+
+    if @reservation.save
+      redirect_to reservation_path(@reservation), notice: '予約が作成されました。'
+    else
+      flash.now[:alert] = @reservation.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
+  def update
+    if @reservation.update(reservation_params)
+      redirect_to reservations_path, notice: '予約が確定しました。'
+    else
+      flash.now[:alert] = @reservation.errors.full_messages.join(', ')
+      render :show
     end
   end
 
@@ -57,7 +55,6 @@ class ReservationsController < ApplicationController
   end
 
   def destroy
-    @reservation = current_user.reservations.find_by(id: params[:id])
     if @reservation
       @reservation.destroy
       redirect_to reservations_path, notice: '予約を削除しました。'
@@ -70,5 +67,10 @@ class ReservationsController < ApplicationController
 
   def reservation_params
     params.require(:reservation).permit(:check_in, :check_out, :number_of_guests)
+  end
+
+  def set_reservation
+    @reservation = Reservation.find_by(id: params[:id])
+    redirect_to reservations_path, alert: '予約が見つかりませんでした。' unless @reservation
   end
 end
